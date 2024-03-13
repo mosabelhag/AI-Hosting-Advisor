8253fcd589cd2efdc52a4b5d92ef87cf661f1c40
D-Marketing <mosabelhag@gmail.com>
2024-03-13 16:56:49 +0200

    Update ui_sample.py

--- ui_sample.py
+++ ui_sample.py
@@ -4,7 +4,7 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 # Now you can access your API keys (and other environment variables)
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 import openai
 import pandas as pd
